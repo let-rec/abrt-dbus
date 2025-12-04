d046ab95aa3f67996a9ca0f52543f5899e30094b
rec <toxtayevhamidulloh6997@gmail.com>
2025-12-04 15:57:03 +0500

    added: start to test d-bus with new project

---
+++ default.nix
@@ -0,0 +1,93 @@
+{
+  pkgs ? let
+    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+    nixpkgs = fetchTarball {
+      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+      sha256 = lock.narHash;
+    };
+  in
+    import nixpkgs {overlays = [];},
+  ...
+}: let
+  # Helpful nix function
+  lib = pkgs.lib;
+  getLibFolder = pkg: "${pkg}/lib";
+  # Manifest via Cargo.toml
+  # manifest = (pkgs.lib.importTOML ./Cargo.toml).workspace.package;
+in
+  pkgs.rustPlatform.buildRustPackage {
+    # Package related things automatically
+    # obtained from Cargo.toml, so you don't
+    # have to do everything manually
+    pname = "abrt-dbus";
+    version = "0.1";
+
+    # Your govnocodes
+    src = pkgs.lib.cleanSource ./.;
+
+    cargoLock = {
+      # lockFile = ./Cargo.lock;
+      # Use this if you have dependencies from git instead
+      # of crates.io in your Cargo.toml
+      # outputHashes = {
+      #   # Sha256 of the git repository, doesn't matter if it's monorepo
+      #   "example-0.1.0" = "sha256-80EwvwMPY+rYyti8DMG4hGEpz/8Pya5TGjsbOBF0P0c=";
+      # };
+    };
+
+    # Compile time dependencies
+    nativeBuildInputs = with pkgs; [
+      # GCC toolchain
+      gcc
+      gnumake
+      pkg-config
+
+      # LLVM toolchain
+      cmake
+      llvmPackages.llvm
+      llvmPackages.clang
+
+      # Rust
+      rustc
+      cargo
+      clippy
+
+      # Other compile time dependencies
+      # postgresql
+    ];
+
+    # Runtime dependencies which will be shipped
+    # with nix package
+    buildInputs = with pkgs; [
+      openssl
+      # libressl
+    ];
+
+    # fixupPhase = ''
+    #   mkdir -p $out/mgrs
+    #   cp -R ./crates/database/* $out/mgrs
+    # '';
+
+    # Set Environment Variables
+    RUST_BACKTRACE = 1;
+    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+    # Compiler LD variables
+    # NIX_LDFLAGS = "-L${(getLibFolder pkgs.libiconv)} -L${(getLibFolder pkgs.postgresql)}";
+    # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    #   pkgs.gcc
+    #   pkgs.libiconv
+    #   pkgs.postgresql
+    #   pkgs.llvmPackages.llvm
+    # ];
+
+    # meta = with lib; {
+    #   homepage = manifest.homepage;
+    #   description = manifest.description;
+    #   # https://github.com/NixOS/nixpkgs/blob/master/lib/licenses.nix
+    #   license = with lib.licenses; [asl20 mit];
+    #   platforms = with platforms; linux ++ darwin;
+    #   mainProgram = "server";
+    #   maintainers = [lib.maintainers.orzklv];
+    # };
+  }
---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # Nix script formatter
+      formatter = pkgs.alejandra;
+
+      # Development environment
+      devShells.default = import ./shell.nix {inherit pkgs;};
+
+      # Output package
+      packages.default = pkgs.callPackage ./. {inherit pkgs;};
+    })
+    // {
+    };
+}
